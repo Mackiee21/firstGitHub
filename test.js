302c8d44abb4da62ef13c0f66e27c30baeb7a609
Mark Anthony Pandac <129565765+Mackiee21@users.noreply.github.com>
2023-04-21 21:37:50 +0800

    added some form for user input

--- test.js
+++ test.js
@@ -1,4 +1,29 @@
-const welcomeUser = user => {
-  console.log(`Hi ${user}`);
+class User{
+  constructor(username, age, gender){
+    this.username = username;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  fullName(){
+    const { username, age, gender} = this;
+    return `My name is ${username}, I am ${age} years old. I am a ${gender}`
+  }
 }
-welcomeUser('Mark');
+const submitBtn = document.querySelector('#submit-btn');
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const username = document.querySelector("#name").value;
+  const age = document.querySelector("#age").value;
+  const genderRadioBtns = document.querySelectorAll("input[type='radio']");
+
+  let gender;
+  genderRadioBtns.forEach(btn => {
+    if(btn.checked){
+      gender = btn.value;
+    }
+  })
+  const user1 = new User(username, age, gender);
+  alert(user1.fullName());
+})
